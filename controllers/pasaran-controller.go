81740bf22211f8eb4261925423a6ee34d3517bb7
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-03-06 07:21:51 +0700

    Check Validation Pasaran

--- controllers/pasaran-controller.go
+++ controllers/pasaran-controller.go
@@ -1575,6 +1575,15 @@ func Pasaransaveconfshio(c *fiber.Ctx) error {
 	if err != nil {
 		log.Println(err.Error())
 	}
+	log.Println("Response Info:")
+	log.Println("  Error      :", err)
+	log.Println("  Status Code:", resp.StatusCode())
+	log.Println("  Status     :", resp.Status())
+	log.Println("  Proto      :", resp.Proto())
+	log.Println("  Time       :", resp.Time())
+	log.Println("  Received At:", resp.ReceivedAt())
+	log.Println("  Body       :\n", resp)
+	log.Println()
 	result := resp.Result().(*response_pasaransave)
 	if result.Status == 200 {
 		c.Status(fiber.StatusOK)
